ad406873474f2a817ad886550154f033952fc389
Christian Mack <20512418+christian-mack@users.noreply.github.com>
2023-10-19 17:50:49 -0400

    camp section complete

--- components/Camp/index.tsx
+++ components/Camp/index.tsx
@@ -1,5 +1,46 @@
+import CampSite from "./CampSite";
+import Image from "next/image";
+
 const Camp = () => {
-  return <div>Camp</div>;
+  return (
+    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          title="Putuk Truno Camp"
+          bgImage="bg-bg-img-1"
+          subtitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite
+          title=" Mountain View Camp"
+          bgImage="bg-bg-img-2"
+          subtitle="Somewhere in the wilderness"
+          peopleJoined="50+ Joined"
+        />
+      </div>
+
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+            <strong>Feeling lost</strong> and not knowing the way?
+          </h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
+            Starting from the anxiety of the climbers when visiting a new
+            climbiung location, the possibility of getting lost is very large.
+            That is why we are here for those of you who want to start an
+            adventure.
+          </p>
+          <Image
+            src="quote.svg"
+            alt="camp-2"
+            width={186}
+            height={219}
+            className="camp-quote"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Camp;
